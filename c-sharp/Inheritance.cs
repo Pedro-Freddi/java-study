decd6d09996140f2b8a9b9c8f395d600d65d881d
Pedro Freddi <pdafreddi@gmail.com>
2023-01-06 17:29:16 -0300

    Add C# notes on interfaces and inheritance

---
+++ c-sharp/Inheritance.cs
@@ -0,0 +1,68 @@
+using System;
+
+namespace inheritance
+{
+  /*
+   * Inheritance is the ability of a class to inherit members of another class.
+   * It provides a way to reduce code duplication and organize the hierarchy of classes.
+   * Nomenclature: a subclass (or derived class) inherits from a superclass (or base class).
+   * Each class can only inherit from one class.
+   * Abstract classes cannot be instantiated.
+   * We can restrict access to a superclass and its subclasses using the 'protected' access modifier.
+   */
+   public abstract class Person
+   {
+    public string Name 
+    { get; protected set; }
+
+    public int Age
+    { get; protected set; }
+
+    public Person(string name, int age)
+    {
+      this.Name = name;
+      this.Age = age;
+    }
+
+    public void HaveBirthday()
+    {
+      this.Age++;
+    }
+
+    // Define a method with the 'virtual' keywork so that subclasses can override
+    // the method if they need to
+    public virtual void SayName()
+    {
+      Console.WriteLine($"My name is {this.Name}.");
+    }
+
+    // Define an abstract method that subclasses must implement
+    public abstract void DoWork();
+
+   }
+
+   // Colon syntax is used to declare that a class inherits from another class
+   public class Paintor : Person
+   {
+    public string FavoriteArtist
+    { get; private set; }
+
+    // Use colon syntax with base() to access the superclass' constructor
+    public Paintor(string name, int age, string favoriteArtist) : base(name, age)
+    {
+      this.FavoriteArtist = favoriteArtist;
+    }
+
+    // Use the 'override' keyword to declare that we're overriding a method of the superclass
+    public override void SayName()
+    {
+      Console.WriteLine($"Hi there! My name is {this.Name} and I'm a paintor.");
+    }
+
+    public override void DoWork()
+    {
+      Console.WriteLine("Painting... painting...");
+    }
+    
+   }
+}
---
+++ c-sharp/Interfaces.cs
@@ -0,0 +1,67 @@
+using System;
+
+namespace interfaces
+{
+  /*
+   * Interfaces define a contract of values and functionality that a class must implement.
+   * They're useful to guarantee certain functionality across multiple classes.
+   * Interfaces can define properties and methods required in a class, but not fields and constructors.
+   * A class can implement multiple interfaces.
+   */
+
+   // An interface is declared with the 'interface' keyword and it's a convention
+   // to start its name with an 'I'
+   interface IAnimal
+   {
+    // Properties
+    string Name { get; }
+    int Age { get; }
+    double Weight { get; }
+    bool IsReptile { get; }
+
+    // Methods
+    void MakeNoise();
+    void Breathe();
+   }
+
+   // A class can declare that implements the interface with the colon syntax
+   class Dog : IAnimal
+   {
+    // Must contain at least the properties and methods defined by the interface
+    public string Name
+    { get; }
+
+    public int Age
+    { get; }
+
+    public double Weight
+    { get; }
+
+    public bool IsReptile
+    { get; }
+
+    public void MakeNoise()
+    {
+      Console.WriteLine("Mmmmmph... Mmmmmph...");
+    }
+
+    public void Breathe()
+    {
+      Console.WriteLine("Breathing in... Breathing out...");
+    }
+
+    // Can contain any other class members
+    public Dog(string name, int age, double weight, bool isReptile)
+    {
+      this.Name = name;
+      this.Age = age;
+      this.Weight = weight;
+      this.IsReptile = isReptile;
+    }
+
+    public void Bark()
+    {
+      Console.WriteLine("AU AU AU AU AU AU");
+    }
+   }
+}
